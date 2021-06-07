efa2beb0be9ffc2fdf7baa8089cf436c3bc99d8f
Chris Miraflor <chris.j.miraflor@gmail.com>
2021-06-06 22:48:39 -0700

    Copied calculator file from other continuous integration activity repo

---
+++ calculator.py
@@ -0,0 +1,19 @@
+"""
+Basic calculator app.
+"""
+
+
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    return a / b

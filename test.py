b16dc1b90216e5a753824a1874107e363419f104
Chris Miraflor <chris.j.miraflor@gmail.com>
2021-06-06 22:56:31 -0700

    Added tests for multiplication and division

--- test.py
+++ test.py
@@ -10,6 +10,11 @@ class TestCase(unittest.TestCase):
     def test_sub(self):
         self.assertEqual(calculator.subtract(4, 2), 2)
 
+    def test_mult(self):
+        self.assertEqual(calculator.multiply(7, 5), 35)
+
+    def test_div(self):
+        self.assertEqual(calculator.divide(9, 3), 3)
 
 if __name__ == "__main__":
     unittest.main()
